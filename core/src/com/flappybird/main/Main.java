2ad4fb485d48bd87fcf1ffe4c6556a2f1bccad5c
doszhan <ffacool5@gmail.com>
2023-05-13 13:15:55 +0600

    changed code

--- core/src/com/flappybird/main/Main.java
+++ core/src/com/flappybird/main/Main.java
@@ -8,16 +8,16 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("sprites/background-day.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
